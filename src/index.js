edd2345d306d59c2790259863b18512fb2978177
Stefano <stefanotamponi@outlook.it>
2020-02-03 12:21:46 +0100

    updated basename, fixed package.json

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 const app = (
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/memoreact">
       <App />
     </BrowserRouter>
   </Provider>
